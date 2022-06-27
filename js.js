4d78aec0b915fd0a99fca431cdb0c81c9fe2111c
VanillaMinazuki <107710812+VanillaMinazuki@users.noreply.github.com>
2022-06-27 20:33:23 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,60 @@
+{
+    "user":[
+        {
+            "id": 1,
+            "name": "Vanilla",
+            "surname": "Tipchuk",
+            "photo_url": "http://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png",
+            "balance": 1500
+        },
+        {
+            "id": 2,
+            "name": "Oleksiy",
+            "surname": "Orlow",
+            "photo_url": "http://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png",
+            "balance": 1300
+        },
+        {
+            "id": 3,
+            "name": "Lew",
+            "surname": "Zubarev",
+            "photo_url": "http://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png",
+            "balance": 1900
+        }
+    ],
+    "products":[
+        {
+            "id": 1,
+            "name": "Відладочна плата Arduino Uno Rev3",
+            "description": "Відладочна плата Arduino Uno Rev3 — це компактна, але при цьому функціональна плата на базі мікроконтролера ATmega328P. ",
+            "price": 245.00,
+            "authorId": 1,
+            "photo_url": "https://www.robostore.com.ua/content/images/29/1500x1500l80mc0/otladochnaja-plata-arduino-uno-rev3-44151274908901.webp"
+        },
+        {
+            "id": 2,
+            "name": "Відладочна плата Arduino BLE-Nano ATMega328 CH340",
+            "description": "Відладочна плата Arduino BLE-Nano ATMega328 CH340 — це компактна функціональна отладочна плата на основі ATmega328 з вбудованим Bluetooth чіпом TI CC2540 BLE на платі. ",
+            "price": 375.00,
+            "authorId": 2,
+            "photo_url": "https://www.robostore.com.ua/content/images/31/1500x1500l80mc0/otladochnaja-plata-arduino-ble-nano-atmega328-ch340-34217149398728.webp"
+        },
+        {
+            "id": 3,
+            "name": "Навчальний набір електронікі на базі Arduino 'RoboStart' для дітей",
+            "description": "Навчальний набір електронікі на базі Arduino 'RoboStart' для дітей — це перший крок до знайомства зі світом робототехніки та високих технологій, за допомогою якого без особливих зусиль зібрати прототип пристроїв вручну, без паяння. ",
+            "price": 1500.00,
+            "authorId": 1,
+            "photo_url": "https://www.robostore.com.ua/content/images/5/1500x1500l80mc0/obuchaushiy-nabor-elektroniki-na-baze-arduino-robostart-dlja-detey-38548199966261.webp"
+        },
+        {
+            "id": 4,
+            "name": "LCD дисплей з синім підсвічуванням 1602 I2C",
+            "description": "LCD дисплей 2004 I2C з синім підсвічуванням — це недорогий екран типу LCD для виведення команд та тексту в візуальний вигляд. ",
+            "price": 97.00,
+            "authorId": 3,
+            "photo_url": "https://www.robostore.com.ua/content/images/24/1500x1500l80mc0/lcd-displey-s-siney-podsvetkoy-1602-i2c-85306435973961.webp"
+        }
+        
+    ]
+}
